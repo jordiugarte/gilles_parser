88ad573ec00b48533e1e16c816ed5675c8dfddc6
jordiugarte <jordi933@gmail.com>
2024-10-13 01:10:27 +0200

    Main class is not compling

--- src/com/gilles/Main.java
+++ src/com/gilles/Main.java
@@ -2,7 +2,17 @@ package com.gilles;
 
 class Main {
 
+    private static String encodingName = "UTF-8";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            java.io.FileInputStream input = new java.io.FileInputStream(args[0]);
+            java.io.Reader reader;
+            reader = new java.io.InputStreamReader(input, encodingName);
+            com.gilles.LexicalAnalyzer lexer = new com.gilles.LexicalAnalyzer(reader);
+            lexer.yylex();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
