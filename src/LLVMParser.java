0a2b333557d1b49a891577ae66add244e078e70d
Jordi Ugarte <jordi933@gmail.com>
2024-12-05 16:33:37 +0100

    Assignations and inputs

--- src/LLVMParser.java
+++ src/LLVMParser.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LLVMParser {
 
-    private StringBuilder finalOutput = new StringBuilder();
-    private Map<String, String> variables = new HashMap<>();
+    private final Map<String, String> variables = new HashMap<>();
 
-    private int tempCount;
+    private int inputCounter;
     private int labelCount;
+
+    private String line() {
+        return line("");
+    }
 
     private String line(String input) {
         return input.concat("\n");
@@ -36,15 +41,18 @@ public class LLVMParser {
         } else {
             // Terminal symbols
             String terminal = expression.split("lexical unit: ")[1];
-            String token = expression.split("lexical unit: ")[0].split("token: ")[1].replaceAll(" ", "");
+            String token = expression.split("lexical unit: ")[0].split("token: ")[1].replaceAll("\\s", "");
             return switch (terminal) {
-                case "[ProgName]" -> "@" + token;
-                case "[VarName]" -> "i32 %" + token;
-                case "[Number]" -> token;
+//                case "[ProgName]" -> "@" + token;
+                case "[ProgName]" -> "@main()";
+                case "[VarName]", "[Number]" -> token;
                 case "LET" -> "define i32 ";
-                case "BE" -> "(";
-                case "END" -> line("}");
+                case "BE" -> line(" {");
+                case "END" -> line("ret i32 0") + line("}");
                 case "COLUMN" -> line("");
+                case "OUT" -> "ret ";
+                case "+" -> " add ";
+                case "=" -> " = ";
                 default -> "";
             };
         }
@@ -52,7 +60,14 @@ public class LLVMParser {
 
     private String program(ParseTree node) {
         // Program -> LET [ProgName] BE <Code> END
-        return generate(node.getChildren().get(0)) +
+        return line("@.str_in = private constant [3 x i8] c\"%d\\00\"") +
+                line("@.str_out = private constant [4 x i8] c\"%d\\n\\00\"") +
+                line("declare i32 @scanf(i8*, ...)") +
+                line("declare i32 @printf(i8*, ...)") +
+                line("%fmt_in = getelementptr [3 x i8], [3 x i8]* @.str_in, i32 0, i32 0") +
+                line("%fmt_out = getelementptr [4 x i8], [4 x i8]* @.str_out, i32 0, i32 0") +
+                line() +
+                generate(node.getChildren().get(0)) +
                 generate(node.getChildren().get(1)) +
                 generate(node.getChildren().get(2)) +
                 line(generate(node.getChildren().get(3))) +
@@ -75,73 +90,58 @@ public class LLVMParser {
 
     private String assign(ParseTree node) {
         // Assign -> VarName = <ExprArith>
-//        String varName = node.getChildren().get(0).getLabel().getValue().toString();
-//        generate(node.getChildren().get(2)); // Traverse <ExprArith>
-//        String tempVar = "%" + (tempCount - 1); // The result of <ExprArith>
-//        String llvmVar = variables.computeIfAbsent(varName, v -> "@" + v);
-//        finalOutput.append("store i32 ").append(tempVar).append(", i32* ").append(llvmVar).append("\n");
-        return "";
+        String var = generate(node.getChildren().getFirst());
+        return line("%" + variableRegistration(var) + generate(node.getChildren().get(1)) + "alloca i32") +
+                line("store i32 " + generate(node.getChildren().get(2)) + ", i32* %" + var) +
+                line("%" + variableRegistration(var) + generate(node.getChildren().get(1)) + "load i32, i32* %" + var);
     }
 
     private String exprArith(ParseTree node) {
         // ExprArith -> <Prod> <ExprArith'>
-//        generate(node.getChildren().get(0)); // generate <Prod>
-//        generate(node.getChildren().get(1)); // generate <ExprArith'>
-        return "";
+        return generate(node.getChildren().get(0)) +
+                generate(node.getChildren().get(1));
     }
 
     private String exprArithPrime(ParseTree node) {
-        // ExprArith' -> + <Prod> <ExprArith'> | epsilon
-//        if (node.getChildren().size() == 3) {
-//            generate(node.getChildren().get(1)); // generate <Prod>
-//            String right = "%" + (tempCount - 1);
-//            String left = "%" + (tempCount - 2);
-//            String result = "%" + tempCount++;
-//            finalOutput.append(result).append(" = add i32 ").append(left).append(", ").append(right).append("\n");
-//            generate(node.getChildren().get(2)); // generate <ExprArith'>
-//        }
+        // ExprArith' -> + <Prod> <ExprArith'> | - <Prod> <ExprArith'> | epsilon
+        if (node.getChildren().size() == 3) {
+            return generate(node.getChildren().get(0)) +
+                    generate(node.getChildren().get(1)) +
+                    generate(node.getChildren().get(2));
+
+        }
         return "";
     }
 
     private String prod(ParseTree node) {
         // Prod -> <Atom> <Prod'>
-//        generate(node.getChildren().get(0)); // generate <Atom>
-//        generate(node.getChildren().get(1)); // generate <Prod'>
-        return "";
+        return generate(node.getChildren().get(0)) +
+                generate(node.getChildren().get(1));
     }
 
     private String prodPrime(ParseTree node) {
         // Prod' -> * <Atom> <Prod'> | epsilon
-//        if (node.getChildren().size() == 3) {
-//            generate(node.getChildren().get(1)); // generate <Atom>
-//            String right = "%" + (tempCount - 1);
-//            String left = "%" + (tempCount - 2);
-//            String result = "%" + tempCount++;
-//            finalOutput.append(result).append(" = mul i32 ").append(left).append(", ").append(right).append("\n");
-//            generate(node.getChildren().get(2)); // generate <Prod'>
-//        }
+        if (node.getChildren().size() == 3) {
+            return generate(node.getChildren().getFirst()) +
+                    generate(node.getChildren().get(1)) +
+                    generate(node.getChildren().get(2));
+        }
         return "";
     }
 
     private String atom(ParseTree node) {
         // Atom -> [Number] | [VarName] | ( <ExprArith> )
-//        if (node.getChildren().get(0).getLabel().getType() == LexicalUnit.NUMBER) {
-//            String number = node.getChildren().get(0).getLabel().getValue().toString();
-//            String result = "%" + tempCount++;
-//            finalOutput.append(result).append(" = add i32 0, ").append(number).append("\n");
-//        } else if (node.getChildren().get(0).getLabel().getType() == LexicalUnit.VARNAME) {
-//            String varName = node.getChildren().get(0).getLabel().getValue().toString();
-//            String llvmVar = variables.computeIfAbsent(varName, v -> "@" + v);
-//            String result = "%" + tempCount++;
-//            finalOutput.append(result).append(" = load i32, i32* ").append(llvmVar).append("\n");
-//        } else {
-//            generate(node.getChildren().get(1)); // generate <ExprArith>
-//        }
-        return "";
+        if (node.getChildren().size() == 1) {
+            return generate(node.getChildren().getFirst());
+        } else {
+            return generate(node.getChildren().getFirst()) +
+                    generate(node.getChildren().get(1)) +
+                    generate(node.getChildren().get(2));
+        }
     }
 
     private String iF(ParseTree node) {
-//        // If -> IF { <Cond> } THEN <Code> END
+        // If -> IF { <Cond> } THEN <Code> END
 //        String trueLabel = "label_" + labelCount++;
 //        String endLabel = "label_" + labelCount++;
 //
@@ -193,22 +193,34 @@ public class LLVMParser {
     }
 
     private String whilE(ParseTree node) {
-        return "";
+//        <While> → WHILE {<Cond>} REPEAT <Code> END
+        return line("while_cond:") +
+                line("while_body:") +
+                generate(node.getChildren().get(5)) +
+                line("while_end:");
     }
 
     private String output(ParseTree node) {
         // Output -> OUT([VarName])
-//        String varName = node.getChildren().get(2).getLabel().getValue().toString();
-//        String llvmVar = variables.get(varName);
-//        String temp = "%" + tempCount++;
-//        finalOutput.append(temp).append(" = load i32, i32* ").append(llvmVar).append("\n")
-//                .append("call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 ")
-//                .append(temp).append(")\n");
-        return "";
+        String var = variableRegistration(generate(node.getChildren().get(2)));
+        return line("call i32 (i8*, ...) @printf(i8* %fmt_out, i32 %" + var + ")");
     }
 
     private String input(ParseTree node) {
         // <Input> → IN ( [VarName] )
-        return "," + generate(node.getChildren().get(2));
+        String var = variableRegistration(generate(node.getChildren().get(2)) + "_input");
+        return line("%" + var + " = alloca i32") +
+                line("call i32 (i8*, ...) @scanf(i8* %fmt_in, i32* %" + var + ")") +
+                line(variableRegistration(var) + generate(node.getChildren().get(1)) + " = load i32, i32* " + var);
+    }
+
+    private String variableRegistration(String var) {
+        // Returns the variable name or its 'val' name if it was already registered
+        if (variables.containsKey(var)) {
+            return variables.get(var);
+        } else {
+            variables.put(var, var.concat("_val"));
+            return var;
+        }
     }
 }
